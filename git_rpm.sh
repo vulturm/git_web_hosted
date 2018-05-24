160ad91ece587ad461ae0ec94c8e10d12b90721b
Mihai Vultur <mihai.vultur@intacct.com>
2018-05-24 20:45:27 +0000

    Add build script for a newer git version

---
+++ git_rpm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p ~/rpmbuild ~/rpmbuild/BUILD ~/rpmbuild/BUILDROOT ~/rpmbuild/RPMS ~/rpmbuild/SOURCES ~/rpmbuild/SPECS ~/rpmbuild/SRPMS
+pushd ~/rpmbuild/SOURCES
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/git-core/git-1.9.0.tar.gz
+rpmbuild -tb git-1.9.0.tar.gz
+
+#yum -y install ~/rpmbuild/RPMS/`uname -i`/git-1.9.0-1.el6.x86_64.rpm ~/rpmbuild/RPMS/`uname -i`/perl-Git-1.9.0-1.el6.x86_64.rpm
